d88152b6e8427611bf3d3826868c606bfc2b1713
Vishwa-Pragnan2004 <156991871+Vishwa-Pragnan2004@users.noreply.github.com>
2025-04-27 22:38:58 +0530

    Add files via upload

---
+++ consts.py
@@ -0,0 +1,32 @@
+"""
+Constants for ConneXion.
+
+Author: Ethan Baker
+"""
+# Interface
+PATH_TO_LOGO = "data/LOGO.jpg"
+# Search Tool
+POSITIONS = []
+LOCATIONS = []
+EXPERIENCE_OPERATOR = "="
+EXPERIENCE_YEARS = 10
+
+REPEAT_QUERIES = False
+API_KEY = "AIzaSyBuOz6EOVnlaSZ_G2ZitOW3k7BMzSlb2zY"
+EXCEL_FILE_LOCATION = "C:\\Users\\vishw\\Desktop\\proj\\connexion-for-linkedin\\test1.xlsx"
+
+API_KEY_EXHAUSTED = "AIzaSyBuOz6EOVnlaSZ_G2ZitOW3k7BMzSlb2zY"
+SEARCH_ENGINE_ID = "137c20153778c4c31"
+PROFILE_INDEX_LOCATION = "search_tool/data/indexed_profiles.pkl"
+QUERY_INDEX_LOCATION = "search_tool/data/indexed_queries.pkl"
+
+# Connection Request Tool
+USERNAME = "vintrovert@outlook.com"
+PASSWORD = "rX9t4N8av-J5n:2"
+
+MESSAGE = ""
+NUM_REQUESTS = ""
+MINIMUM_CONNECTION_COUNT = ""
+
+EXCEL_INPUT_LOCATION = "C:\\Users\\vishw\\Desktop\\proj\\connexion-for-linkedin\\test1.xlsx"
+
---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+The primary application script for ConneXion.
+
+"""
+from interface import Interface
+
+# Application code
+if __name__ == '__main__':
+    interface = Interface()
+    interface.run()
